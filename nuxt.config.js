1c14a2898ec51d5949941043aa3fe717408abaf5
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2023-08-30 17:27:52 +0200

    netlify 404 fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
+  generate: {
+    fallback: true
+  },
   head: {
     titleTemplate: '%s - invoice-app',
     title: 'invoice-app',
